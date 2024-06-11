579812a8346b54ffba0a6f2011ee049a51303b66
Tomi Pineiro <tomaspineiro1@outlook.es>
2024-06-11 19:00:31 -0300

    Agrego un objeto con nombre de alumno y un vector con todas sus notas

--- js/main.js
+++ js/main.js
@@ -4,14 +4,32 @@ let NotasTotales = 0;
 let cantNotas = 0;
 let notaPromedioFinal = 0;
 
+const vectorNotas = [];
+const objetoAlumno = {
+    nombre: "",
+    notas: vectorNotas,
+};
+
+
 let Alumno = prompt("ingrese el nombre de un alumno");
 if (Alumno != "Nadie"){
+    objetoAlumno.nombre= Alumno;
     let notas = Number(prompt("Ingrese la nota de una materia"));
     while (notas != 0){
+        vectorNotas.push(notas);
         cantNotas ++;
         NotasTotales += notas;
         notas = Number(prompt("Ingrese la nota de una materia"));
     }
+    
+    objetoAlumno.notas = vectorNotas;
+
 }
 
 alert(notaPromedioFinal = NotasTotales / cantNotas);
+
+console.log(objetoAlumno.nombre);
+
+for (let i=0; i < vectorNotas.length; i++){
+    console.log(objetoAlumno.notas[i]);
+}
