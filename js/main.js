ac8e78a0a7011aaf3582c3dae69afc5858af4e03
Tomas Pineiro <tomaspineiro1@outlook.es>
2024-06-25 17:10:49 -0300

    Agregue funcion leerNotas y calcularPromedio, la funcion es la misma pero con funciones

--- js/main.js
+++ js/main.js
@@ -4,32 +4,42 @@ let NotasTotales = 0;
 let cantNotas = 0;
 let notaPromedioFinal = 0;
 
+
+// declaracion estructuras y funciones
 const vectorNotas = [];
 const objetoAlumno = {
     nombre: "",
     notas: vectorNotas,
 };
 
-
-let Alumno = prompt("ingrese el nombre de un alumno");
-if (Alumno != "Nadie"){
-    objetoAlumno.nombre= Alumno;
-    let notas = Number(prompt("Ingrese la nota de una materia"));
+function leerNotas(notas){
     while (notas != 0){
         vectorNotas.push(notas);
         cantNotas ++;
         NotasTotales += notas;
         notas = Number(prompt("Ingrese la nota de una materia"));
     }
-    
-    objetoAlumno.notas = vectorNotas;
-
 }
 
-alert(notaPromedioFinal = NotasTotales / cantNotas);
+function calcularPromedio(){
+    let Alumno = prompt("ingrese el nombre de un alumno");
+    if (Alumno != "Nadie"){
+        objetoAlumno.nombre = Alumno;
+        let notas = Number(prompt("Ingrese la nota de una materia"));
+        leerNotas(notas);
+    }
+    objetoAlumno.notas = vectorNotas;
+}
+
+
+// llamada de funciones
+calcularPromedio();
 
 console.log(objetoAlumno.nombre);
 
 for (let i=0; i < vectorNotas.length; i++){
     console.log(objetoAlumno.notas[i]);
 }
+
+
+alert(notaPromedioFinal = NotasTotales / cantNotas);
