9a7846b17e1b131d2a18d3212d34fd6228e391d9
Tomas Pineiro <tomaspineiro1@outlook.es>
2024-07-01 02:17:36 -0300

    Se añaden botones y casillas con interactividad javascript

--- js/main.js
+++ js/main.js
@@ -4,42 +4,100 @@ let NotasTotales = 0;
 let cantNotas = 0;
 let notaPromedioFinal = 0;
 
+// declaracion funciones
 
-// declaracion estructuras y funciones
-const vectorNotas = [];
-const objetoAlumno = {
-    nombre: "",
-    notas: vectorNotas,
+const objetoBoletin = {
+    nombreMateria: "",
+    notaMateria: 0,
 };
 
-function leerNotas(notas){
-    while (notas != 0){
-        vectorNotas.push(notas);
-        cantNotas ++;
-        NotasTotales += notas;
-        notas = Number(prompt("Ingrese la nota de una materia"));
-    }
+const vectorBoletin = [objetoBoletin];
+
+const objetoAlumno = {
+    nombre: "",
+    boletin: vectorBoletin,
+};
+
+// function leerNotas(notas){
+//     while (notas != 0){
+//         vectorNotas.push(notas);
+//         cantNotas ++;
+//         NotasTotales += notas;
+//         notas = Number(prompt("Ingrese la nota de una materia"));
+//     }
+// }
+
+// function calcularPromedio(){
+//     let Alumno = prompt("ingrese el nombre de un alumno");
+//     if (Alumno != "Nadie"){
+//         objetoAlumno.nombre = Alumno;
+//         let notas = Number(prompt("Ingrese la nota de una materia"));
+//         leerNotas(notas);
+//     }
+//     objetoAlumno.notas = vectorNotas;
+// }
+
+function ingresarDatosBoletin (materia, nota){
+    objetoBoletin.nombreMateria = materia;
+    objetoBoletin.notaMateria = nota;
 }
 
-function calcularPromedio(){
-    let Alumno = prompt("ingrese el nombre de un alumno");
-    if (Alumno != "Nadie"){
-        objetoAlumno.nombre = Alumno;
-        let notas = Number(prompt("Ingrese la nota de una materia"));
-        leerNotas(notas);
+// function mostrarDatosBoletin (vectorBoletin,i){
+//     alert("Nombre de la materia: " + vectorBoletin[i].nombreMateria + " " + "Nota de la materia: " + vectorBoletin[i].notaMateria);
+// };
+
+// function recorrerBoletin (){
+//     for (let i = 0; i < vectorBoletin.length; i++){
+//         console.log("Nombre de la materia: " + vectorBoletin[i].nombreMateria + " " + "Nota de la materia: " + vectorBoletin[i].notaMateria);
+//     }; 
+// };
+
+// function recorrerBoletin() {
+//     for (let i = 0; i < vectorBoletin.length; i++) {
+//         alert(`Nombre de la materia: ${vectorBoletin[i].nombreMateria} - Nota: ${vectorBoletin[i].notaMateria}`);
+//     }
+// }
+
+function recorrerBoletin() {
+    let mensaje = "";
+    for (let i = 0; i < vectorBoletin.length; i++) {
+        mensaje += `Nombre de la materia: ${vectorBoletin[i].nombreMateria} - Nota: ${vectorBoletin[i].notaMateria}\n`;
     }
-    objetoAlumno.notas = vectorNotas;
+    alert(mensaje);
 }
-
-
 // llamada de funciones
-calcularPromedio();
+//calcularPromedio();
 
-console.log(objetoAlumno.nombre);
+// for (let i=0; i < vectorNotas.length; i++){
+//     console.log(objetoAlumno.notas[i]);
+// }
 
-for (let i=0; i < vectorNotas.length; i++){
-    console.log(objetoAlumno.notas[i]);
-}
+// fin algoritmos y llamados 
 
+const input01 = document.getElementById("floatingInputNombre");
+const input02 = document.getElementById("floatingInputMateria");
+const input03 = document.getElementById("floatingInputNota");
 
-alert(notaPromedioFinal = NotasTotales / cantNotas);
+const btnEnviar = document.getElementById("btnEnviar");
+
+btnEnviar.addEventListener("click", () => {
+    const nombre = input01.value;
+    const materia = input02.value;
+    const nota = input03.value;
+    if (input01.value === "" || input02.value === "" || input03.value === "Elegir la nota correspondiente"){
+        alert("Hay campos que no fueron ingresados");
+    }else{
+        objetoAlumno.nombre = nombre;
+        const nuevoBoletin = {
+            nombreMateria: materia,
+            notaMateria: nota,
+        };
+        vectorBoletin.push(nuevoBoletin);
+    }
+});
+
+const btnTerminar = document.getElementById("btnTerminar");
+
+btnTerminar.addEventListener("click", () => {
+    recorrerBoletin();
+});
